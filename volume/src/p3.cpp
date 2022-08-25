03b53dadef5c969ef5b352a72a43caf63538d35e
educscs <109014596+educscs@users.noreply.github.com>
2022-08-25 10:20:16 -0400

    Update p3.cpp

--- volume/src/p3.cpp
+++ volume/src/p3.cpp
@@ -1,43 +1,86 @@
 #include <iostream>
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <cstring>
+#include <climits>
+
 using namespace std;
+
 struct Alumno
-{  
-  string Nombre;
-  string Apellidos;
-  string Carrera;
-  float mensualidad;
-};
-ostream &operator<<(ostream &stream, Alumno &alumno)
 {
-    stream << alumno.Nombre << '|';
-    stream << alumno.Apellidos << '|';
-    stream << alumno.Carrera << '|';
-    stream << to_string(alumno.mensualidad);
+    string Nombre;
+    string Apellidos;
+    string Carrera;
+    float mensualidad;
+
+    void print()
+    {
+        cout << Nombre << endl
+             << Apellidos << endl
+             << Carrera << endl
+             << mensualidad << endl;
+    }
+};
+
+ostream &operator<<(ostream &stream, Alumno &p)
+{
+    cout << "Begin \n";
+    stream << p.Nombre << '|';
+    stream << p.Apellidos << '|';
+    stream << p.Carrera << '|';
+    stream << to_string(p.mensualidad);
     stream << "\n";
     stream << flush;
     return stream;
 }
 
-class VariableRecord{
-  string filename;
+class VariableRecord
+{
+private:
+    string filename;
 
-  VariableRecord(string filename){
-    this->filename=filename;
-  }
-  vector<Alumno> load(){
-     
-      
-      while(getline)
-    
-  }
-  void add(Alumno record)
+public:
+    VariableRecord(string filename)
+    {
+        this->filename = filename;
+    }
+
+    vector<Alumno> load()
+    {
+        ifstream File(this->filename, ios::in);
+        vector<Alumno> alumnos;
+        if (!File.is_open())
+            cout << "No se pudo abrir el archivo. \n";
+
+        string line;
+
+        getline(File, line);
+
+        while (getline(File, line))
+        {
+            char *data = const_cast<char *>(line.c_str());
+            data = strtok(data, "|");
+            Alumno record;
+
+            record.Nombre = data;
+            data = strtok(NULL, "|");
+
+            record.Apellidos = data;
+            data = strtok(NULL, "|");
+
+            record.Carrera = data;
+            data = strtok(NULL, "|");
+
+            record.mensualidad = atof(data);
+            data = strtok(NULL, "|");
+
+            alumnos.push_back(record);
+        }
+
+        return alumnos;
+    }
+    void add(Alumno record)
     {
         ofstream File(this->filename, ios::app | ios::out);
         if (!File.is_open())
@@ -48,12 +91,46 @@ class VariableRecord{
         File.close();
     }
 
+    Alumno readRecord(int pos)
+    {
+        ifstream File(this->filename, ios::app | ios::in);
+        if (!File.is_open())
+            cout << "No se pudo abrir el archivo. \n";
+
+        string line;
+
+        getline(File, line);
+
+        for (int i = 0; i < pos; ++i)
+            getline(File, line);
+
+        char *data = const_cast<char *>(line.c_str());
+        data = strtok(data, "|");
+        Alumno record;
+
+        record.Nombre = data;
+        data = strtok(NULL, "|");
+
+        record.Apellidos = data;
+        data = strtok(NULL, "|");
+
+        record.Carrera = data;
+        data = strtok(NULL, "|");
+
+        record.mensualidad = atoi(data);
+        data = strtok(NULL, "|");
+
+        return record;
+    }
 };
 
-
-
-int main() {
-  std::cout << "Hello World!\n";
+int main()
+{
+    VariableRecord file("datos3.txt");
+    Alumno alumno1;
+    alumno1 = file.readRecord(1);
+    alumno1.print();
+    return 0;
 }
 
 int p3Test() {
